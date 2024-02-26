ca7cb9e19811c50db00aea9bafe53604eda1e801
Ankcorn <thomasankcorn@gmail.com>
2024-02-26 11:41:01 +0000

    modify redis persistance

--- drain3/redis_persistence.py
+++ drain3/redis_persistence.py
@@ -1,26 +1,37 @@
 # SPDX-License-Identifier: MIT
 
-import redis
+import redis as Redis
 
 from drain3.persistence_handler import PersistenceHandler
 
 
 class RedisPersistence(PersistenceHandler):
-    def __init__(self, redis_host, redis_port, redis_db, redis_pass, is_ssl, redis_key):
-        self.redis_host = redis_host
-        self.redis_port = redis_port
-        self.redis_db = redis_db
-        self.redis_pass = redis_pass
-        self.is_ssl = is_ssl
+    def __init__(self, redis_host, redis_port, redis_db, redis_pass, is_ssl, redis_key, redis):
         self.redis_key = redis_key
-        self.r = redis.Redis(host=self.redis_host,
-                             port=self.redis_port,
-                             db=self.redis_db,
-                             password=self.redis_pass,
-                             ssl=self.is_ssl)
+
+        if isinstance(redis, Redis):
+            self.r = redis
+        else:
+            self.r = Redis(host=redis_host,
+                                port=redis_port,
+                                db=redis_db,
+                                password=redis_pass,
+                                ssl=is_ssl)
 
     def save_state(self, state):
         self.r.set(self.redis_key, state)
 
     def load_state(self):
         return self.r.get(self.redis_key)
+
+
+# persistance = RedisPersistence(
+#     redis= Redis(
+#         host='localhost',
+#         port=6379,
+#         db=0,
+#         password=None,
+#         ssl=False
+#     ),
+#     redis_key='drain3'
+# )
